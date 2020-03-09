45f5333b6cbcf16423003aa299620a55c7ad527c
doYoungAn <jispoop@gmail.com>
2020-03-10 00:35:13 +0900

    react-native-wifi-reborn link

--- App.tsx
+++ App.tsx
@@ -16,7 +16,8 @@ import {
   View,
   Text,
   StatusBar,
-  PermissionsAndroid
+  PermissionsAndroid,
+  BackHandler
 } from 'react-native';
 
 import {
@@ -28,6 +29,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import DeviceInfo from 'react-native-device-info';
+import WifiManager from "react-native-wifi-reborn";
 
 declare var global: {HermesInternal: null | {}};
 
@@ -35,19 +37,42 @@ const App = () => {
 
   useEffect(() => {
     console.log('init App');
-    DeviceInfo.getAndroidId().then((id) => {
-      console.log(`id - ${id}`)
-    });
-    PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE
-    ).then((value) => {
-      console.log(`value - ${value}`);
-      DeviceInfo.getPhoneNumber().then((num) => {
-        console.log(`num - ${num}`);
-      });
-    }).catch((err) => {
-
-    });
+    (async () => {
+      const checkPhoneState: boolean = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE);
+      const fineLocation: boolean = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+      if (!checkPhoneState || !fineLocation) {
+        const request = await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+        ]);
+        console.log(JSON.stringify(request))
+        console.log('이건?', PermissionsAndroid.RESULTS.GRANTED)
+        if (request["android.permission.READ_PHONE_STATE"] !== PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('in1');
+          BackHandler.exitApp();
+        }
+        if (request["android.permission.ACCESS_FINE_LOCATION"] !== PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('in2');
+          BackHandler.exitApp();
+        }
+      }
+      const deviceId = await DeviceInfo.getAndroidId();
+      console.log(`device id - ${deviceId}`);
+      const phoneNum = await DeviceInfo.getPhoneNumber();
+      console.log(`phone - ${phoneNum}`);
+      WifiManager.loadWifiList(
+        wifiList => {
+            let wifiArray =  JSON.parse(wifiList);
+            wifiArray.map((value: any, index: any) =>
+                {
+                  console.log(`Wifi ${index  +  1} - ${value.SSID}`);
+                  // console.log(JSON.stringify(value));
+                }
+            );
+        },
+        error =>  console.log(error)
+      );
+    })();
   }, []);
 
   return (
